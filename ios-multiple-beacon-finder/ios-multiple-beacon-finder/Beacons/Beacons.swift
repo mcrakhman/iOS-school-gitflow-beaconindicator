13a096b6ef318688d59e6408564ca038195141cf
Mikhail Rakhmanov <m.rakhmanov@rambler-co.ru>
2016-12-11 02:45:24 +0300

    Добавлена структура, описывающая маячок

---
+++ ios-multiple-beacon-finder/ios-multiple-beacon-finder/Beacons/Beacons.swift
@@ -0,0 +1,23 @@
+//
+//  Beacons.swift
+//  ios-multiple-beacon-finder
+//
+//  Created by m.rakhmanov on 11.12.16.
+//  Copyright © 2016 m.rakhmanov. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Beacon {
+    let name: String
+    let uuid: NSUUID
+    let majorValue: CLBeaconMajorValue
+    let minorValue: CLBeaconMinorValue
+}
+
+func ==(beacon: Beacon, clbeacon: CLBeacon) -> Bool {
+    return ((clbeacon.proximityUUID.uuidString == beacon.uuid.uuidString)
+        && (Int(clbeacon.major) == Int(beacon.majorValue))
+        && (Int(clbeacon.minor) == Int(beacon.minorValue)))
+}
